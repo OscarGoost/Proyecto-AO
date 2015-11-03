591757beb51e0f7227c8ab1543c3dd9a09f14279
Arroncero <antonioronceroabalos@gmail.com>
2015-11-03 18:50:51 +0100

    Toma Gatos y Tetas, gayyyy

---
+++ proyect_GT.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  proyect_GT.c
+ *
+ *    Description:  Gatos y tetas versión ASCII
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    int i;
+
+            for (i;i<=100000;i++){
+                if (i%2==0)
+                    system ("toilet -F border --gay -f pagga \"Gatos y Tetas\"");
+                else
+                    system ("toilet -F border --gay -f pagga \"Tetas y Gatos\"");
+	    }
+
+	return EXIT_SUCCESS;
+}
