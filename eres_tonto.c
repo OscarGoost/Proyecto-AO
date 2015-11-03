d1b1f88cc3a35ff2b09cea03a46421935d49d773
OscarGoost <oscarszuwarowski@gmail.com>
2015-11-03 21:07:06 +0100

    podras no ser tonto?

---
+++ eres_tonto.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int i=0;
+
+    while (i==0)
+        system("zenity --question --text \"¿es usted tonto?\"");
+
+    return EXIT_SUCCESS;
+}
