09da0334d4101bd0e8c1c454393245e89fb8af3f
OscarGoost <oscarszuwarowski@gmail.com>
2015-11-04 19:51:31 +0100

    version de proyect_GT.c sin comentarios

--- gif_GT.c
+++ gif_GT.c
@@ -1,25 +1,10 @@
-/*
- * =====================================================================================
- *
- *       Filename:  proyect_GT.c
- *
- *    Description:  Gatos y tetas versión ASCII
- *
- * =====================================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main (){
-
     int i;
 
-    /* Realmente no ha sido nada dificil, solo he tenido que cambiar
-    los printf del programa que ya hicimos en la uni por la orden system y sale, pero está gracioso
-    Ah! y no se te curra hacer la gracia de poner "i<= un número grande", se peta muy jarcor */
-
-            for (i;i<=100;i++){
+        for (i;i<=100;i++){
                 if (i%2==0){
                     system ("clear");
                     system ("toilet -F border --gay -f pagga \"Gatos y Tetas\"");
