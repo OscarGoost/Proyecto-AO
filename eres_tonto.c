f651dfd01744af4ed5021c332173e3fea163db01
Arroncero <antonioronceroabalos@gmail.com>
2015-11-05 22:21:01 +0100

    la solución de Txema no ha funcionado :c

--- eres_tonto.c
+++ eres_tonto.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (){
-    int i=0;
+int main (){;
 
-    while (i==0)
-        system("zenity --question --text \"¿es usted tonto?\"");
+   int ans;
+
+   ans = system("zenity --question --text \"¿es usted tonto?\"; echo $?");
+
+   if (ans = 1){
+       do{
+	  system("zenity --question --text \"seguro?\"");
+       }while (ans = 1);
+   }
+   else
+       system("zenity --info --text `toilet -F border - pagga \"TONTO\"`");
+
 
     return EXIT_SUCCESS;
 }
